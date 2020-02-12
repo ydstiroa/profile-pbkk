41175305e468407230b4409a9bedc186a25ba384
ydstiroa <47847548+ydstiroa@users.noreply.github.com>
2020-02-12 14:43:06 +0700

    PBKK

---
+++ application/views/home/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Biodata</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md  fixed-top maine-menu">
+    <div class="container">
+      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"> <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
+      <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#about" tabindex="-1" aria-disabled="true">About</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#activity" tabindex="-1" aria-disabled="true">Experience</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#sosmed" tabindex="-1" aria-disabled="true">Social Media</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid fh5co-home-banner">
+    <div class="card"> <img class="card-img" src="assets/img/foto.jpg">
+      <div class="card-img-overlay">
+        <div class="center-text">
+         <i> <h2 class="card-title" >Biodata</h2> </i>
+     
+            <svg width="201" height="51" viewBox="0 0 201 51">
+              <defs>
+                <style>
+                .cls-1 {
+                  fill: none;
+                  stroke-width: 1px;
+                  stroke: url(#linear-gradient);
+                }
+              </style>
+              <linearGradient id="linear-gradient" x1="140.508" y1="50.5" x2="60.492" y2="0.5" gradientUnits="userSpaceOnUse">
+                <stop offset="0" stop-color="#e90e65"/>
+                <stop offset="1" stop-color="#fff"/>
+              </linearGradient>
+            </defs>
+            
+          </svg>
+       
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid fh5co-two-img">
+    <div class="row">
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+      <img src="assets/img/foto_saya.jpg" alt="foto saya" height="100%" width="100%" style="display: block; margin: auto;">
+      </div>
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+        
+          <div class="content" style="color: white; font-family:sans-serif" id="about">
+            <h3>About</h3>
+            <hr color="white">
+            <p>Nama     : Yudhistiro Adi Nugroho</p>
+            <p>Alamat   : Keputih Tegal Timur II No.36</p>
+            <p>Universitas  : Institut Teknologi Sepuluh Nopember</p>
+            <p>Email : yudhistiro56@gmail.com</p>
+            <p>HP : 081388481520</p>
+            <br>
+            <br>
+            <p>Biasa dipanggil Yudhis adalah seorang mahasiswa di departemen Teknik Informatika ITS angkatan 2017</p>        
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid fh5co-two-img">
+    <div class="row">
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+      <img src="assets/img/work.png" alt="foto saya" height="100%" width="100%" style="display: block; margin: auto;">
+      </div>
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+        
+          <div class="content" style="color: white; font-family:sans-serif" id="activity">
+            <h3>Experience</h3>
+            <hr color="white">
+            <li>Ketua Departemen Media Informasi Forda Baduy</li>
+            <li>Staff Humas Schematics 2018</li>
+            <li>Staff Ahli Humas Schematics 2019</li>
+            <li>Administrator Laboratorium Manajemen Informasi</li>
+            <li>Staff Media Informasi HMTC</li>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid fh5co-insta-feed activity" id="sosmed">
+    <div class="container recent">
+      <h2 class="text-center d-block">Find me on social media</h2>
+      <div class="row social-links">
+        <ul class="nav mx-auto">
+          <li class="nav-item"> <a class="nav-link" href="https://www.facebook.com/yudhistiro.adi"><img src="assets/img/facebook.png" alt=""></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="http://github.com/ydstiroa"><img src="assets/img/Github1.png" alt=""></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="https://www.instagram.com/yudhistiroa/"><img src="assets/img/ig.png" alt=""></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <footer class="container-fluid p-0 pr-0">
+    <div class="copy pt-4 pb-4">
+      <p><a href="https://freehtml5.co/" target="_blank"> &copy; 2020 Yudhis</a></p>
+    </div>
+  </footer>
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS --> 
+  <script src="assets/js/jquery-3.3.1.slim.min.js"></script> 
+  <script src="assets/js/popper.min.js" ></script> 
+  <script src="assets/js/bootstrap.min.js"></script> 
+  <script src="assets/js/owl.carousel.min.js"></script> 
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
